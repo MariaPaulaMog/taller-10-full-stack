0c91b3fd197bd822d59aa22326bf0abeec0b1e4b
mpaulamr30@gmail.com <mpaulamr30@gmail.com>
2023-07-20 19:54:49 -0500

    primer commit

---
+++ punto-1/punto-1.php
@@ -0,0 +1,23 @@
+<?php
+// CONSTANTS
+const PI = 3.1415;
+define("WELCOME_MESSAGE", "Bienvenido a PHP");
+
+// VARIABLES
+$firstName = "Maria";
+$lastName = "Mogollon";
+
+echo PI . "\n";
+echo WELCOME_MESSAGE . "\n";
+echo $firstName . " " . $lastName . "\n";
+
+$firstName = "Andres";
+$lastName = "Barreto"; // Se agregó el punto y coma (;) al final de esta línea.
+
+echo "- - - - - - - - - - -\n";
+
+echo PI . "\n";
+echo WELCOME_MESSAGE . "\n";
+echo $firstName . " " . $lastName . "\n";
+
+?>
